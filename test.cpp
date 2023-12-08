db0ef5667f010e3759da87f83224ee7e0dece794
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 16:35:26 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -16,3 +16,21 @@ int main()
   }
     return 0;
 }
+std::string isPalindrome(int n)
+{
+	int remainder,reversed_number=0;
+	  while(n != 0) {
+    remainder = n % 10;
+    reversed_number = reversed_number * 10 + remainder;
+    n /= 10;
+  }
+	if (n == reversed_number) {
+		// Return "Yes"
+		return "Yes";
+	}
+	// Otherwise
+	else {
+		// return "No"
+		return "No";
+	}
+}
