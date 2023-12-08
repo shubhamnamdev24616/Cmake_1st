6f1ff36383de9b9590576b144af309817968084b
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 12:24:32 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "calc.h"
+//#include "calc.h"
+using namespace std;
+int main()
+{
+   
+	if(isPalindrome(S)== "yes")
+  {
+  cout<<"test cases is pass"<<endl;
+  }
+  else
+  {
+   cout<<"test cases is fail"<<endl;
+  }
+    return 0;
+}
