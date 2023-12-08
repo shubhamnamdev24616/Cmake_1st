ea9d73ff7d1074d3a4efc42db70826791c994530
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 16:37:45 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,10 +3,20 @@
 std::string isPalindrome(int n);
 int main()
 {
-   int n= 1234321;
+   int n= 1234321,n1=24616;
 	std::string Result;
 	Result=isPalindrome(n);
 	if(Result== "Yes")
+  {
+  std::cout<<"test cases is pass"<<std::endl;
+  }
+  else
+  {
+   std::cout<<"test cases is fail"<<std::endl;
+  }
+	std::string Result1;
+	Result1=isPalindrome(n);
+	if(Result1== "Yes")
   {
   std::cout<<"test cases is pass"<<std::endl;
   }
@@ -18,13 +28,13 @@ int main()
 }
 std::string isPalindrome(int n)
 {
-	int remainder,reversed_number=0;
+	int n1=n,remainder,reversed_number=0;
 	  while(n != 0) {
     remainder = n % 10;
     reversed_number = reversed_number * 10 + remainder;
     n /= 10;
   }
-	if (n == reversed_number) {
+	if (n1 == reversed_number) {
 		// Return "Yes"
 		return "Yes";
 	}
