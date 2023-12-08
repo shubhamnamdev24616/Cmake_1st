2ac10cfc91fc0989eccb9ca28f167677f5c3afb9
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 16:38:51 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -2,13 +2,13 @@
 
 std::string isPalindrome(int n)
 {
-	int remainder,reversed_number=0;
+	int n1=n,remainder,reversed_number=0;
 	  while(n != 0) {
     remainder = n % 10;
     reversed_number = reversed_number * 10 + remainder;
     n /= 10;
   }
-	if (n == reversed_number) {
+	if (n1 == reversed_number) {
 		// Return "Yes"
 		return "Yes";
 	}
