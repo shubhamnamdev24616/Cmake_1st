48663b5ff4f3177f4b2abe9841a0ae37b222b3fb
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 14:40:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 int main()
 {
    std::int S = 1234321,S1=24616;
-	std::int result = isPalindrome(S);
+	std::string result = isPalindrome(S);
         std::cout << result;
-	std::int result2 = isPalindrome(S1);
+	std::string result2 = isPalindrome(S1);
         std::cout << result2;
 	
     return 0;
