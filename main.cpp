fe04d4afe852143fc92dcfaa620c8fc4fe556c94
shubhamnamdev24616 <152868222+shubhamnamdev24616@users.noreply.github.com>
2023-12-08 12:35:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<string>
 #include "calc.h"
 //#include "calc.h"
 using namespace std;
 int main()
 {
    string S = "ABCDCBA",S1="shubham";
-	cout << isPalindrome(S);
-	cout << isPalindrome(S1);
+	std::string cout << isPalindrome(S);
+	std::string cout << isPalindrome(S1);
     return 0;
 }
